1749a6fe04fd12d6e75eb3f289f109a8a9779aba
Your Name <you@example.com>
2025-03-12 13:52:23 +0200

    first commit

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -1,16 +1,9 @@
 import ForHome from "./ForHome";
-// import Gpt4 from "../gpt4/page";
-// import CaseStudies from "../caseStudies/page";
-// import OpenAi from "../openAi/page";
-// import Library from "../library/page";
+
 const page = () => {
   return (
     <div className="">
       <ForHome />
-      {/* <Gpt4 />
-      <CaseStudies />
-      <OpenAi />
-      <Library /> */}
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Home from "./home/page";
-
+import Gpt4 from "../gpt4/page";
+import CaseStudies from "../caseStudies/page";
+import OpenAi from "../openAi/page";
+import Library from "../library/page";
 const page = () => {
   return (
     <div className="container mx-auto">
       <Home />
+      <Gpt4 />
+      <CaseStudies />
+      <OpenAi />
+      <Library />
     </div>
   );
 };
